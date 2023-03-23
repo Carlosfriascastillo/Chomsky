df7b94515dd6dd12727ae5d142a4c96bb7abe733
Carlosfriascastillo <c.friascastillo@gmail.com>
2023-03-23 12:20:46 -0600

    sectionchallenge

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+    int main()
+    {
+        int favorite_number;
+        
+        std::cout <<"Escoje tu numero favorito entre el 1 y el 100:";
+        
+        std::cin >>favorite_number;
+        
+        std::cout<<"!Increible! ese es el mio " <<std::endl;
+        
+        std::cout<<"realmente no, " <<favorite_number << " no lo es" <<std::endl;
+        return 0;
+        
+        }
